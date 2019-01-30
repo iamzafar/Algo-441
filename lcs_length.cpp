5792bb8b8d62fc9e89b75f1df7d384dacfb3f819
inson <mzafar2@umbc.edu>
2019-01-30 16:26:22 -0500

    added changes

--- lcs_length.cpp
+++ lcs_length.cpp
@@ -93,32 +93,16 @@ int getLCS( char * String1, int m, char * String2, int n )
     matrix[i] =  new int[n+m]; 
    }
 
-   #pragma omp parallel for
-   for (int a = 0; a <= m; ++a)
-   {
-     /* code */
-    matrix[a][0] = 0;
-   }
-
-   #pragma omp parallel for
-   for (int b = 0; b <= n; ++b)
-   {
-     /* code */
-    matrix[0][b] = 0;
-   }
-
-   //std::cout << "======here=======" << std::endl;
-   /* Following steps build matrix[m+1][n+1] in bottom up fashion. Note 
-      that matrix[i][j] contains length of LCS of string1[0..i-1] and string2[0..j-1] */
+      /* Following steps build matrix[m+1][n+1] in bottom up fashion. Note 
+      that matrix[i][j] contains length of LCS of string1[0..i-1] and string2[0..j-1] */   
    
-   double t0 = omp_get_wtime();
-   
+   double t0 = omp_get_wtime();   // get the initial time
    for (int i=1; i<=m; ++i)
    {
       #pragma omp parallel for shared(matrix)
       for (int j=1; j<=n; ++j){
-       // if (i == 0 || j == 0)
-       //   matrix[i][j] = 0;
+       if (i == 0 || j == 0)
+         matrix[i][j] = 0;
   
        if (String1[i-1] == String2[j-1])
          matrix[i][j] = matrix[i-1][j-1] + 1;
@@ -132,8 +116,8 @@ int getLCS( char * String1, int m, char * String2, int n )
      }
    }
     
+  // calculate diagonal 
   int weight = 0;
-
    for (int k = 2; k <= m; ++k){
      if(weight < m-n){
         weight +=1;
